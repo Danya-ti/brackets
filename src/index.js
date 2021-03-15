7c625c170927c13534780e128d8dc8b82b347c88
Danya-ti <danya.nschnts@gmail.com>
2021-03-16 01:02:03 +0300

    feat: add solution

--- src/index.js
+++ src/index.js
@@ -1,3 +1,35 @@
-module.exports = function check(str, bracketsConfig) {
-  // your solution
+module.exports = function check(str, bracketsConfig)
+{
+  let pairObj = {};
+  let brackets = str.split("");
+  let openBrack = [];
+  let result = true;
+
+  bracketsConfig.forEach(pair => {
+    pairObj[pair[1]] = pair[0];
+  });
+
+  brackets.forEach(element => {
+    if (pairObj[element] === element) {
+      if (element === openBrack[openBrack.length - 1]) {
+        openBrack.pop();
+      } else {
+        openBrack.push(element);
+      }
+    } else if (!pairObj[element]) {
+      openBrack.push(element);
+    } else {
+      if (pairObj[element] === openBrack[openBrack.length - 1]) {
+        openBrack.pop();
+      } else {
+        result = false;
+      }
+    }
+  });
+
+  if (openBrack.length !== 0) {
+    result = false;
+  }
+
+  return result;
 }
